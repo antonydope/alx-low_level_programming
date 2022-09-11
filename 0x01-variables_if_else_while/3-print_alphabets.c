90faaa55c6b3cbc7220fcd01adbbce24d7e959f4
antonydope <antonymusumba6@gmail.com>
2022-09-10 23:55:29 -0700

    task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: p
+ * Return: 0
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+for (ch = 'A' ; CH <= 'Z' ; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
