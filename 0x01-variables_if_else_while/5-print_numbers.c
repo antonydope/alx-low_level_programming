e6d305b65104b688f143a11c52314519c38091cf
antonydope <antonymusumba6@gmail.com>
2022-09-11 00:24:12 -0700

    task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: p
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 0 ; i < 10 ; i++)
+{
+printf("%i", 1)
+}
+printf("\n");
+return (0);
+}
