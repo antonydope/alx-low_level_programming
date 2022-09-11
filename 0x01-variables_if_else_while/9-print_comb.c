0ca67dfe331328d17944df36d56b6da06acb92a7
antonydope <antonymusumba6@gmail.com>
2022-09-11 01:12:40 -0700

    task

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description:
+ * Return: 0
+ */
+int main(void)
+{
+int c;
+for (c = 48; c <= 57; c++)
+{
+putchar(c);
+if (c != 57)
+{
+putchar(',');
+putchat(' ');
+}
+}
+putchar('\n');
+return (0);
+}
