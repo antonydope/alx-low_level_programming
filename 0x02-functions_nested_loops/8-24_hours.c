6e8888e90f2e35017a782d599c6b78b92c430909
antonydope <antonymusumba6@gmail.com>
2022-09-14 08:19:30 -0700

    task

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,7 +9,7 @@ void jack_bauer(void)
 int hours = 0;
 int minutes = 0;
 int hours_remainder;
-int mins_remainder:
+ int mins_remainder;
 while (hours <= 23)
 {
 while (minutes <= 59)
@@ -22,7 +22,7 @@ _putchar(':');
 _putchar(minutes / 10 + '0');
 _putchar(mins_remainder + '0');
 minutes++;
-_putchar('\n);
+_putchar('\n');
 }
 hours++;
 minutes = 0;
