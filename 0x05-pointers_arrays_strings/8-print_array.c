e7b89fccb13f844b411db004a71b07c8088eeb63
antonydope <antonymusumba6@gmail.com>
2022-09-19 02:48:11 -0700

    task

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *print_array - prints n elements of an array of integers 
+ *print_array - prints n elements of an array of integers
  *@a: argument
  *@n: argument
  */
