e67004bb710a73b06abd3db125af7d0ad44bc435
antonydope <antonymusumba6@gmail.com>
2022-09-21 23:09:49 -0700

    task

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *string_toupper - lowercase to uppercase
+ *@str: the string to be changed
+ *Return: a pointer to the string to be changed
+ */
+char *string_toupper(char *str)
+{
+int index = 0;
+while (str[index])
+{
+if (str[index] >= 'a' && str[index] <= 'z')
+str[index] -= 32;
+index++;
+}
+return (str);
+}
