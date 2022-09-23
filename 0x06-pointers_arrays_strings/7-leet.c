0dadef8b3b76884176de24c74cec1b0d7e2521fd
antonydope <antonymusumba6@gmail.com>
2022-09-22 18:09:45 -0700

    task

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,14 +8,14 @@ char *leet(char *str)
 {
 int indx1 = 0, indx2;
 char leet[8] = {'0', 'L', '?', 'E', 'A', '?', '?', 'T'};
-while (str[indx1])
+while (str[++indx1])
 {
 for (indx2 = 0; indx2 <= 7; indx2++)
 {
-if (str[indx1 == leet[indx2] || str[indx1] - 32 == leet[indx2])
+if (str[indx1] == leet[indx2] ||
+str[indx1] - 32 == leet[indx2])
 str[indx1] = indx2 + '0';
 }
-index1++;
 }
 return (str);
 }
